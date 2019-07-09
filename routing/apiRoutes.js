c1731bc321b27ff22b26935626f00f8fe2883ebf
Trevor Fairbank <Trevorfairbank92@gmail.com>
2019-07-08 21:10:21 -0700

    fixed app.post to target correct friend

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -13,54 +13,44 @@ module.exports = (app) => {
     app.post("/api/friends", (req, res) => {
         // console.log(req.body.scores);
         let newFriendScores = [];
-        let matchName = "";
-        let matchImage = "";
-        let diffArr = [];
-        let bestMatch= 40;
-        //loops through req.body (bunch of strings) and makes them numbers
-        for (var i = 0; i < req.body.scores.length; i++) {
-            newFriendScores.push(+req.body.scores[i]);
-        }
 
-        // console.log(newFriendScores);
 
-        //loops through all different friends in friends.js
+        let bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: Infinity
+        };
+
+        const userData = req.body;
+        const userScores = userData.scores;
+
+        let totalDifference;
+
+        // Here we loop through all the friend possibilities in the database.
         for (var i = 0; i < friendsData.length; i++) {
-            let totalDifference = 0;
-            //loops through and calculates the difference between all the friends.
-            for (var x = 0; x < newFriendScores.length; x++) {
-                totalDifference += Math.abs(friendsData[i].scores[x] - newFriendScores[x]);
+            var currentFriend = friendsData[i];
+            totalDifference = 0;
+            // console.log(currentFriend.name);
+            // We then loop through all the scores of each friend
+            for (var j = 0; j < currentFriend.scores.length; j++) {
+                var currentFriendScore = currentFriend.scores[j];
+                var currentUserScore = userScores[j];
+                // We calculate the difference between the scores and sum them into the totalDifference
+                totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
             }
-            //pushes the differences to an empty array.
-            diffArr.push(totalDifference);
-            //sorts the array from small to high
-            diffArr.sort(function (a, b) { return a - b });
-            //variable for the first index of array
-            const lowestDiff = diffArr[0];
-
-    //Couldn't figure out how to target the Lowest Difference and link that back to the friend with that score...
-            if(lowestDiff <= 19 && lowestDiff >= 0){
-                matchName = friendsData[i].name;
-                matchImage = friendsData[i].photo;
-            }
-            else if (lowestDiff > 20){
-                matchName = friendsData[0].name;
-                matchImage = friendsData[0].photo;
+            // If the sum of differences is less then the differences of the current "best match"
+            if (totalDifference <= bestMatch.friendDifference) {
+                // Reset the bestMatch to be the new friend.
+                bestMatch.name = currentFriend.name;
+                bestMatch.photo = currentFriend.photo;
+                bestMatch.friendDifference = totalDifference;
             }
         }
-
-
-        // Add user to friends.js
-        friendsData.push(req.body);
-
-        res.json(
-            {
-                name: matchName,
-                photo: matchImage,
-                friendsData
-
-            }
-        )
+        // Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
+        // the database will always return that the user is the user's best friend).
+        friendsData.push(userData);
+        // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
+        res.json(bestMatch);
 
     });
 };
